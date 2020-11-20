4554cd651ec7d8ff0b9745fa5d26105cef72ee5e
Joakim Anderlind <joakimanderlind@gmail.com>
2020-11-20 10:42:11 +0000

    refactored error handling

--- src/app.js
+++ src/app.js
@@ -20,12 +20,14 @@ app.get('/balance', (req,res) => {
 		return
 	}
 
-	const name = req.query.name;
-	if(accounts.has(name)){
-		res.send(`current account balance for ${name}: ${accounts.get(name)}`);
-	}else{
-		res.status(404).send("No account found...");
+	if(!accounts.has(name)){
+		res.status(404).send("No account found...")
+		return
 	}
+
+	const name = req.query.name
+	const balance = accounts.get(name)
+	res.send(`current account balance for ${name}: ${balance}`);
 })
 /*
 
@@ -93,12 +95,12 @@ app.get('/deposit', (req,res) => {
 app.get('/transfer', (req,res) => {
 
 	if(!req.query.to){
-		res.status(400).send("Please specify the name of the account")
+		res.status(400).send("Please specify the name of the sender")
 		return
 	}
 
 	if(!req.query.from){
-		res.status(400).send("Please specify the name of the account")
+		res.status(400).send("Please specify the name of the recipient")
 		return
 	}
 
@@ -111,24 +113,28 @@ app.get('/transfer', (req,res) => {
 	const to = req.query.to
 	const amount = parseInt(req.query.amount)
 
-	if(accounts.has(from) && accounts.has(to)){
-		const from_balance = accounts.get(from)
-		if(amount <= from_balance){
-			const new_from_balance = from_balance - amount;
-			const new_to_balance = accounts.get(to) +  amount;
-
-			accounts.set(from, new_from_balance);
-			accounts.set(to, new_to_balance);
-
-			res.send(`The amount of ${amount} has been transfered from ${from} to ${to}`)
-			return
-		}
+	if(!accounts.has(from) || !accounts.has(to)){
+		res.status(404).send("No account found...")
+		return
+	}
+	
+	const from_balance = accounts.get(from)
+	const to_balance = accounts.get(to)
+	
+	if(amount > from_balance){
 		res.status(400).send(`The payee does not have the balance to transfere amount`)
 		return
 	}
-	res.status(404).send("No account found...")
+		
+	const new_from_balance = from_balance - amount;
+	const new_to_balance = to_balance +  amount;
+
+	accounts.set(from, new_from_balance);
+	accounts.set(to, new_to_balance);
+
+	res.send(`The amount of ${amount} has been transfered from ${from} to ${to}`)
 })
 
 app.listen(port, () => {
-	console.log(`SafeBank is running on http://localhost:${port}`);
+	console.log(`SafeBank is running on http://localhost:${port}`)
 })
