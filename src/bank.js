51098865f6c054574fed4af62ee0d4f45d9da2c9
Joakim Anderlind <joakimanderlind@gmail.com>
2020-11-20 10:42:31 +0000

    refactored out model to seperate file

---
+++ src/bank.js
@@ -0,0 +1,46 @@
+class Bank {
+	constructor(init) {
+		this.accounts = init
+	}
+
+	iter_accounts() {
+		return this.accounts.entries()
+	}
+
+	account_exists(name){
+		return this.accounts.has(name)
+	}
+
+	account_balance(name) {
+		return this.accounts.get(name)
+	}
+
+	account_change_balance(name,fn){
+		const current_balance = this.accounts.get(name)
+		const new_balance = fn(current_balance)
+		if(new_balance >= 0){
+			this.accounts.set(name, fn(current_balance))
+			return true;
+		}
+		else{
+			return true;
+		}
+	}
+
+	account_deposit(name,value){
+		return this.account_change_balance(name, (x) => x + value)
+	}
+
+	account_withdraw(name,value){
+		return this.account_change_balance(name, (x) => x -= value)
+	}
+
+	account_transfer(from,to,value){
+		if(this.account_withdraw(from,value)){
+			return this.account_deposit(to,value)
+		}
+		return false;
+	}
+}
+
+exports.bank = new Bank(new Map([["adam",20],["eve",3000],["mega-evil-corp",999999]]))
