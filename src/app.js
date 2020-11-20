61a25a30f7802ba1075e0267e372bcb350bb1c76
Joakim Anderlind <joakimanderlind@gmail.com>
2020-11-20 10:50:07 +0000

    swiched to external bank module

--- src/app.js
+++ src/app.js
@@ -1,13 +1,8 @@
 const express = require('express')
+const SafeBank = require('./bank.js')
 const app = express()
+const bank = SafeBank.bank;
 const port = 3000;
-
-let accounts = new Map()
-
-accounts.set('adam',3000)
-accounts.set('eve',3020)
-accounts.set('mega-corp',99999)
-
 
 app.get('/', (req,res) => {
 	res.sendFile(__dirname + "/client.html");
@@ -20,78 +15,18 @@ app.get('/balance', (req,res) => {
 		return
 	}
 
-	if(!accounts.has(name)){
+	const name = req.query.name
+
+	if(!bank.account_exists(name)){
 		res.status(404).send("No account found...")
 		return
 	}
 
-	const name = req.query.name
-	const balance = accounts.get(name)
+	const balance = bank.account_balance(name)
 	res.send(`current account balance for ${name}: ${balance}`);
 })
-/*
-
-	withdraw and deposit is overkill?
 
 
-app.get('/withdraw', (req,res) => {
-
-	if(!req.query.name){
-		res.status(400).send("Please specify the name of the account")
-		return
-	}
-
-	if(!req.query.amount || isNaN(req.query.amount)){
-		res.status(400).send("Please specify amount")
-		return
-	}
-
-
-	const name = req.query.name
-	const amount = parseInt(req.query.amount)
-
-	if(accounts.has(name)){
-		const balance = accounts.get(name)
-
-		if(amount <= balance){
-			const new_balance = balance - amount;
-			accounts.set(name,new_balance);
-			res.send(`The withdrawal of ${amount} from account ${name} was successful.`)
-			return
-		}
-		else{
-			res.status(400).send("Error: withdrawal amount larger than current balance")
-			return
-		}
-	}
-	res.status(404).send("No account found...")
-})
-
-app.get('/deposit', (req,res) => {
-
-	if(!req.query["name"]){
-		res.status(400).send("Please specify the name of the account")
-		return
-	}
-
-	if(!req.query.amount || isNaN(req.query.amount)){
-		res.status(400).send("Please specify amount")
-		return
-	}
-
-	const name = req.query.name
-	const amount = parseInt(req.query.amount)
-
-	if(accounts.has(name)){
-		const new_balance = accounts.get(name) + amount;
-		accounts.set(name,new_balance);
-		res.send(`The deposit of ${amount} from account ${name} was successfull`)
-		return
-	}
-	res.status(404).send("No account found...")
-})
-
-*/
 app.get('/transfer', (req,res) => {
 
 	if(!req.query.to){
@@ -113,24 +48,17 @@ app.get('/transfer', (req,res) => {
 	const to = req.query.to
 	const amount = parseInt(req.query.amount)
 
-	if(!accounts.has(from) || !accounts.has(to)){
+	if(!bank.account_exists(from) || !bank.account_exists(to)){
 		res.status(404).send("No account found...")
 		return
 	}
 	
-	const from_balance = accounts.get(from)
-	const to_balance = accounts.get(to)
+	const transfer_result = bank.account_transfer(from,to,amount)
 	
-	if(amount > from_balance){
-		res.status(400).send(`The payee does not have the balance to transfere amount`)
+	if(!transfer_result){
+		res.status(400).send(`The account does not have the balance to transfer amount`)
 		return
 	}
-		
-	const new_from_balance = from_balance - amount;
-	const new_to_balance = to_balance +  amount;
-
-	accounts.set(from, new_from_balance);
-	accounts.set(to, new_to_balance);
 
 	res.send(`The amount of ${amount} has been transfered from ${from} to ${to}`)
 })
